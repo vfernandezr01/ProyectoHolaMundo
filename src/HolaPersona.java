420be2fdff0bbaacb7ee5a7880496ba292d3cebd
vfernandezr01 <vfernandezr01@educarex.es>
2023-11-10 12:25:07 +0100

    modificado

--- src/HolaPersona.java
+++ src/HolaPersona.java
@@ -1,4 +1,14 @@
+import javax.swing.JOptionPane;
 
 public class HolaPersona {
 
+	
+	public static void main(String[] args) {
+		
+		String nombre=JOptionPane.showInputDialog("Dame tu nombre: ");
+		
+		JOptionPane.showMessageDialog(null, "hola" + nombre);
+		
+		System.out.println("Hola caracola");
+	}
 }
